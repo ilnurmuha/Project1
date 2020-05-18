bf06b5418afff1bf2ab734f2d872a2abf68fc760
ilnur <60211406+ilnurmuha@users.noreply.github.com>
2020-05-18 23:21:47 +0500

    Add files via upload

---
+++ Project1/js/script.js
@@ -0,0 +1,53 @@
+let receiveBtnOne = document.getElementById('receiveOne'),
+	receiveBtnTwo = document.getElementById('receiveTwo'),
+	receiveBtnThree = document.getElementById('receiveThree'),
+	receiveBtnFour = document.getElementById('receiveFour'),
+	receiveBtnFive = document.getElementById('receiveFive'),
+	receiveBtnSix = document.getElementById('receiveSix'),
+	receiveBtnSeven = document.getElementById('receiveSeven'),
+	receiveBtnEight = document.getElementById('receiveEight'),
+	receiveBtnNine = document.getElementById('receiveNine'),
+	modal = document.querySelector('.modal'),
+	closeBtn = document.querySelector('.close');
+
+
+
+receiveBtnOne.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnTwo.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnThree.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnFour.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnFive.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnSix.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnSeven.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnEight.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+receiveBtnNine.addEventListener('click', function(){ // click - событие клика
+	modal.style.display = 'flex'; // обращаемся к модальному окну.получаем доступ к стилям.стиль display меняем на block
+});
+
+closeBtn.addEventListener('click', function(){ // закрываем модальное окно 
+	modal.style.display = 'none'; // делаем display: none, как было первоначально
+});
